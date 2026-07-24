9ba8450149891151739a15bbf6bf0dc0ab5d729a
Precieux <ton_email_github@example.cohttps://github.com/precieuxjusticek-tech/travio-backend.git>
2026-07-24 03:42:38 +0100

    touhjours correction firebase

--- firebase.js
+++ firebase.js
@@ -1,9 +1,11 @@
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_KEY.replace(/\\n/g, '\n')
-);
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
