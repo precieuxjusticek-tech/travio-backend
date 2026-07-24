418a8bdfe4db8ba8ff0ebf320cb090f25f6b0cf2
Precieux <ton_email_github@example.cohttps://github.com/precieuxjusticek-tech/travio-backend.git>
2026-07-24 03:17:14 +0100

    fix firebase env render

--- firebase.js
+++ firebase.js
@@ -1,16 +1,13 @@
 const admin = require('firebase-admin');
-const path  = require('path');
 require('dotenv').config();
 
-// Initialisation Firebase Admin avec la clé privée
-const serviceAccount = require(path.resolve(process.env.FIREBASE_KEY_PATH));
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-// On exporte auth et firestore pour les utiliser dans server.js
-const auth      = admin.auth();
+const auth = admin.auth();
 const firestore = admin.firestore();
 
 module.exports = { auth, firestore };
